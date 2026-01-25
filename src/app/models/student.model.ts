1158e1913ccb868d1abbceae5e0d5d7aa64d3ff1
Alexsander Campos <ext-alexsander.boas@bbts.com.br>
2026-01-25 15:47:18 -0300

    Student submit form

--- src/app/models/student.model.ts
+++ src/app/models/student.model.ts
@@ -2,7 +2,7 @@ export interface Student {
   id: number;
   name: string;
   email?: string;
-  enrolledCourses?: number[];
+  enrolledCourseId?: number[];
   registrationDate?: string;
   phoneNumber?: string;
 }
